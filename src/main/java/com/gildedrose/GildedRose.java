23843e7ab6734e2bcf1d4c1b5808f9ddc511bdee
manuel <mahanifl@iastate.edu>
2024-09-23 17:44:35 +0200

    clean up

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,29 +9,6 @@ class GildedRose {
         this.itemUpdateHandler = new ItemUpdateHandler();
     }
 
-    // increase, decrease Quality functions
-    // create static variables for names of exception items
-    // use switch for exceptions i.e. Aged Brie, Sulfuras, Backstage passes, "Conjured" items and otherwise
-    // or check if enum value matches
-    // process item normally
-    // Check for backstage pass item is hardcoded, needs to be more dynamic i.e. startsWith "Backstage passes"
-    // create unit tests for all cases
-    // (possibly create interface for accessing Items class
-
-
-    // every item decreases sellIn date (except Sulfuras)
-    // Aged Brie increases quality per day passed
-    // Sulfuras sellIn and quality stay same
-    // Backstage passes increase quality like Aged Brie
-    // except: if sellIn < 11 then quality +2, if sellIn < 6 then quality +3, after sellIn < 0 then quality = 0
-    // "Conjured XY" quality decreases -2 per day
-    // Item can not have quality above 50, except Sulfuras fixed at 80 always
-
-
-    // Aged Brie
-    // Backstage passes to a TAFKAL80ETC concert
-    // Sulfuras, Hand of Ragnaros
-
     public void updateQuality() {
         // Changed to for-each loop
         for (Item item : items) {
